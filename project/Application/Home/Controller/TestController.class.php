a7671c2ffccfdc1acdfb35730983b7edea12a3fd
root <zhaochang45@126.com>
2015-10-11 01:16:10 +0800

    change

--- project/Application/Home/Controller/TestController.class.php
+++ project/Application/Home/Controller/TestController.class.php
@@ -3,6 +3,27 @@ namespace Home\Controller;
 use Think\Controller;
 class TestController extends Controller {
     public function index() {
+        $id = $_GET['id'];
+        $due = false;
+        if($id) {
+            $itemsMdl = D('Items');
+            $id = \Common\Lib\Idhandler::decode($id);
+            $item = $itemsMdl->getRow(array('id' => $id));
+            $itemsMdl->saveData(array('id' => $id, 'visits' => $item['visits'] + 1));
+            $this->assign('item', $item);
+            if($item['due_at'] < time()) {
+                $due = true;
+            }
+        }
+
+        $this->assign('due', $due);
+
+        $app = '';
+        $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+        if(strpos($agent, 'iphone')) {
+            $app ='ios';
+        }
+        $this->assign('app', $app); 
         $this->display();
     }
 }
